3c88a7074ea3eb901a4332b2ac656fb653d7e418
Dyandra Allen <dallen9@binghamton.edu>
2019-08-29 11:07:04 -0400

    completed assignment01

--- assignment01/SimpleDateTester.java
+++ assignment01/SimpleDateTester.java
@@ -8,31 +8,25 @@ public class SimpleDateTester {
     try(var output =new PrintWriter(new FileOutputStream(
 			    new File("output.txt"), true /* true means append to file */))) {
 			         output.println("\nTESTS FOR SimpleDate.java:");
-               SimpleDate date = new SimpleDate();
-               SimpleDate date2 = new SimpleDate();
-               SimpleDate date3 = new SimpleDate();
-               SimpleDate date4 = new SimpleDate();
-               SimpleDate date5 = new SimpleDate();
-               SimpleDate date6 = new SimpleDate();
 
-               var testDate = date.of(2000, 11, 23);
-               var testDate2 = date2.of(2001, 12, 24);
-               var testDate3 = date3.of(2000, 11, 21);
-               var testDate4 = date4.of(2000, 12, 23);
-               var testDate5 = date5.of(1999, 11, 23);
-               var testDate6 = date6.of(2000, 11, 23);
+				SimpleDate date = SimpleDate.of(2000,11,23);
+				SimpleDate date2 = SimpleDate.of(2001, 12, 24);
+				SimpleDate date3 = SimpleDate.of(2000, 11, 21);
+				SimpleDate date4 = SimpleDate.of(2000, 12, 23);
+				SimpleDate date5 = SimpleDate.of(1999, 11, 23);
+				SimpleDate date6 = SimpleDate.of(2000, 11, 23);
 
-               System.out.println(testDate.before(testDate2)); //true
-               System.out.println(testDate.before(testDate3)); //false
-               System.out.println(testDate.before(testDate4)); //true
-               System.out.println(testDate.before(testDate5)); //false
-               System.out.println(testDate.before(testDate6)); //false
+               System.out.println(date.before(date2)); //true
+               System.out.println(date.before(date3)); //false
+               System.out.println(date.before(date4)); //true
+               System.out.println(date.before(date5)); //false
+               System.out.println(date.before(date6)); //false
 
-               output.println(testDate.before(testDate2));
-               output.println(testDate.before(testDate3));
-               output.println(testDate.before(testDate4));
-               output.println(testDate.before(testDate5));
-               output.println(testDate.before(testDate6)); 
+               output.println(date.before(date2));
+               output.println(date.before(date3));
+               output.println(date.before(date4));
+               output.println(date.before(date5));
+               output.println(date.before(date6)); 
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
